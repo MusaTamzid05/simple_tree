a75d3ff8bce1421c966a91ae7a0bdec2a0b66bf4
musa <tamzid101@gmail.com>
2022-01-14 00:02:02 +0600

    ok its looking something like a ta tree.

--- main.rb
+++ main.rb
@@ -4,11 +4,14 @@ def get_dirname_from(path)
   path.split(File::SEPARATOR)[-1]
 end
 
-def traverse(dir_path)
+def traverse(dir_path, space_count = 0)
   dir_name = get_dirname_from(dir_path)
   entries = Dir.entries(dir_path).sort
 
-  puts dir_name
+  if space_count == 0
+    puts dir_name
+  end
+
   middle_sign = "├── "
   end_sign = "└── "
 
@@ -22,7 +25,19 @@ def traverse(dir_path)
     else
       sign = end_sign 
     end
-    puts "#{sign} #{entry}"
+
+    space_count.times do |space_index|
+      print "\t"
+    end
+
+
+    puts "#{sign} #{entry} #{space_count}"
+
+    current_path = File.join(dir_path, entry)
+
+    if File.directory?(current_path)
+      traverse(current_path, space_count + 1)
+    end
   end
 end
 
