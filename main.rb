be751ba0b79a72d3d766b1381b1d71968e501672
musa <tamzid101@gmail.com>
2022-01-14 00:17:16 +0600

    deep count.

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ def get_dirname_from(path)
   path.split(File::SEPARATOR)[-1]
 end
 
-def traverse(dir_path, space_count = 0)
+def traverse(dir_path, space_count = 0, deep_count = 0)
   dir_name = get_dirname_from(dir_path)
   entries = Dir.entries(dir_path).sort
 
@@ -28,7 +28,7 @@ def traverse(dir_path, space_count = 0)
     end
 
     space_count.times do |space_index|
-      if space_index == space_count - 1
+      if space_index == space_count - deep_count
         print line_sign
       end
 
@@ -41,7 +41,7 @@ def traverse(dir_path, space_count = 0)
     current_path = File.join(dir_path, entry)
 
     if File.directory?(current_path)
-      traverse(current_path, space_count + 1)
+      traverse(current_path, space_count + 1, deep_count + 1)
     end
   end
 end
