85cfb932fdd44fb0419106ed1bb7df8203f35345
musa <tamzid101@gmail.com>
2022-01-14 02:11:36 +0600

    tree bug for nested structure fixed.

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ def get_dirname_from(path)
   path.split(File::SEPARATOR)[-1]
 end
 
-def traverse(dir_path, space_count = 0, deep_count = 0)
+def traverse(dir_path, space_count = 0,  sign_index_list = [])
   dir_name = get_dirname_from(dir_path)
   entries = Dir.entries(dir_path).sort
 
@@ -28,26 +28,30 @@ def traverse(dir_path, space_count = 0, deep_count = 0)
     end
 
     space_count.times do |space_index|
-      if space_index == space_count - deep_count
-        print line_sign
+      if sign_index_list.include? space_index
+          print line_sign
       end
 
       print "      "
     end
 
-
     puts "#{sign} #{entry}"
-
     current_path = File.join(dir_path, entry)
 
     if File.directory?(current_path)
-      traverse(current_path, space_count + 1, deep_count + 1)
+      next_index_list = sign_index_list.clone
+
+      if index < entries.length - 1
+        next_index_list.append(space_count)
+      end
+      
+      traverse(current_path, space_count + 1, next_index_list )
     end
   end
 end
 
 def main
-  traverse("./test")
+  traverse("./test2")
 end
 
 main
